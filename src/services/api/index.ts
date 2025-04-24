1b63f294fabae92713c0f3aae6188d36a79a3a77
williamberg123 <williamberg567@gmail.com>
2025-04-24 18:47:34 -0300

    atualizando a burrada

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://localhost:5555/v1/',
+    baseURL: process.env.NODE_ENV === 'development'
+	? 'http://localhost:5555/v1/'
+	: 'https://bergmovies-api.onrender.com/v1',
 });
